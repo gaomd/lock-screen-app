3dae1c8ed0bb6184eba50d3b9dc0874cf60327cb
windgazer <martin@windgazer.nl>
2014-07-16 12:29:22 +0200

    Adding new lock screen method that works without nasty side-effects in OSX Mavericks

---
+++ lock screen/Lock Screen Bundle.applescript
@@ -0,0 +1,2 @@
+set lockscreen to POSIX path of (path to resource "lockscreen" in directory "Scripts") as string
+do shell script quoted form of lockscreen
--- lock screen/main.m
+++ lock screen/main.m
@@ -1,6 +1,7 @@
 #import <objc/runtime.h>
 #import <Foundation/Foundation.h>
 
+//Code found on http://apple.stackexchange.com/questions/80058/lock-screen-command-one-liner
 int main () {
     NSBundle *bundle = [NSBundle bundleWithPath:@"/Applications/Utilities/Keychain Access.app/Contents/Resources/Keychain.menu"];
 
